e3ea9da4ed2930204c6b01931910e44a3785c52d
alectian <alecmail@icloud.com>
2019-03-25 16:27:24 +0800

    创建mysql

---
+++ API4/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"Server/API4/config"
+	"Server/API4/router"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfg = pflag.StringP(
+		"config",             //名称
+		"c",                  //速记
+		"config/config.yaml", //配置文件路径
+		"config file usage",
+	)
+)
+
+func main() {
+	/*
+		在 main 函数中增加了 config.Init(*cfg) 调用，
+		用来初始化配置，
+		cfg 变量值从命令行 flag 传入，
+		可以传值，
+		比如 ./apiserver -c config.yaml，
+		也可以为空，
+		如果为空会默认读取 conf/config.yaml。
+	*/
+	pflag.Parse()
+
+	err := config.Init(*cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	engine := gin.Default()
+	middlewares := []gin.HandlerFunc{}
+	router.Load(engine, middlewares...)
+	addr := viper.GetString("addr")
+	fmt.Print(viper.GetString("alec.name"))
+	http.ListenAndServe(addr, engine)
+}
+
+
